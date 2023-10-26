df98398f1a47f7a0059d3e3ab7ca09c8bce38644
Chukwuka Onyekachukwu <chukwukaonyekachukwu@measurabl.com>
2023-10-26 12:51:37 +0100

    first init

---
+++ src/main/java/com/dexspace/openaiconnector/OpenaiConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.dexspace.openaiconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dexspace/openaiconnector/OpenaiConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexspace.openaiconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
